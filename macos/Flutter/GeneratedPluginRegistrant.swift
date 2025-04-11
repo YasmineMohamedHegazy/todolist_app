2416fa77a8792829624ea92f197704e4be29d3da
YasmineMohamedHegazy <Yasmin.Hassan@student.aast.edu>
2025-04-11 04:44:29 +0200

    Updated files

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -4,23 +4,9 @@
 
 import FlutterMacOS
 import Foundation
-import path_provider_foundation
-
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-
-import cloud_firestore
-import firebase_core
-import path_provider_foundation
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-
 
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-}}
+}
