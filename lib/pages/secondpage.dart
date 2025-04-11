087e3b9f14625c2530a9b00dc34b9df6e3d641c0
YasmineMohamedHegazy <Yasmin.Hassan@student.aast.edu>
2025-04-11 04:57:10 +0200

    Recover deleted files from commit 093949e

---
+++ lib/pages/secondpage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Profile Page",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+    );
+  }
+}
